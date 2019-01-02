ae9d04c52d7213d0bf02e4141d57bd716bc4f83e
joder <974318723@qq.com>
2019-01-02 22:54:39 +0800

    update

---
+++ core-web/src/main/resources/static/js/panel.js
@@ -0,0 +1,24 @@
+$(function () {
+    $("#span-tips-hide").tooltip('show');
+    $("#span-tips-show").tooltip('hide');
+
+    $("#span-tips-hide").on('click',function () {
+        $("#operator-panel").hide(500);
+        setTimeout(function () {
+            $("#span-tips-hide").tooltip('hide');
+            $("#span-tips-show").tooltip('show');
+        },500)
+
+    })
+
+
+
+    $("#span-tips-show").on('click',function () {
+        $("#operator-panel").show(500);
+        setTimeout(function () {
+            $("#span-tips-hide").tooltip('show');
+            $("#span-tips-show").tooltip('hide');
+        },500);
+
+    })
+})
